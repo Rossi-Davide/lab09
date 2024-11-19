0e962100573e303574a5e4846e8bb93b43a9d6c8
Davide Rossi - davide.rossi47@studio.unibo.it <davide.rossi47@studio.unibo.it>
2024-11-19 16:55:49 +0100

    Fix eol error

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,16 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +18,47 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private Controller controller;
+    private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI(final Controller controller) {
+
+        this.controller = controller;
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout()); 
+        final JTextArea textInput = new JTextArea();
+        canvas.add(textInput, BorderLayout.CENTER);
+        final JButton saveButton = new JButton("Save");
+        canvas.add(saveButton,BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        saveButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    controller.writeString(textInput.getText());
+                }catch (IOException ex){
+                    JOptionPane.showMessageDialog(frame, ex, "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI(new Controller()).display();
+    }
 
 }
