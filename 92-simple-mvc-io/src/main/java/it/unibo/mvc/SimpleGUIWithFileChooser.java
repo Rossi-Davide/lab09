cd93eaf5cb1b7e722f4659e646ca894441710e8c
Davide Rossi - davide.rossi47@studio.unibo.it <davide.rossi47@studio.unibo.it>
2024-11-19 18:00:19 +0100

    Implement file chooser

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,8 +1,11 @@
 package it.unibo.mvc;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -16,10 +19,11 @@ public final class SimpleGUIWithFileChooser {
 
     private final JFrame frame = new JFrame();
 
-    public SimpleGUIWithFileChooser(){
+    public SimpleGUIWithFileChooser(final Controller controller){
         final JPanel innerTopPanel = new JPanel();
         innerTopPanel.setLayout(new BorderLayout());
         final JTextField textField = new JTextField();
+        textField.setEditable(false);
         final JButton browseButton = new JButton("Browse");
         innerTopPanel.add(textField, BorderLayout.CENTER);
         innerTopPanel.add(browseButton,BorderLayout.LINE_END);
@@ -28,6 +32,22 @@ public final class SimpleGUIWithFileChooser {
         mainPanel.add(innerTopPanel, BorderLayout.NORTH);
         frame.setContentPane(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+         * Handlers
+         */
+        browseButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final JFileChooser fileChooser = new JFileChooser();
+                final int res = fileChooser.showSaveDialog(mainPanel); 
+                if (res == JFileChooser.APPROVE_OPTION){
+                    controller.setCurrentFile(fileChooser.getSelectedFile());
+                    textField.setText(controller.getPath());
+                }
+            }
+            
+        });
     }
 
 
@@ -38,7 +58,7 @@ public final class SimpleGUIWithFileChooser {
     }
 
     public static void main(String[] args) {
-        new SimpleGUIWithFileChooser().display();
+        new SimpleGUIWithFileChooser(new Controller()).display();
     }
 
 }
