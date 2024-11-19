bbee6fbac86cf6432cebd5c1530e84433fb2c207
Davide Rossi - davide.rossi47@studio.unibo.it <davide.rossi47@studio.unibo.it>
2024-11-19 17:01:51 +0100

    Implement ui in simpleGuiFileChooser

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,6 +1,11 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 /**
@@ -10,5 +15,17 @@ import javax.swing.JFrame;
 public final class SimpleGUIWithFileChooser {
 
     private final JFrame frame = new JFrame();
+
+    public SimpleGUIWithFileChooser(){
+        final JPanel innerTopPanel = new JPanel();
+        innerTopPanel.setLayout(new BorderLayout());
+        final JTextField textField = new JTextField();
+        final JButton browseButton = new JButton();
+        innerTopPanel.add(textField, BorderLayout.CENTER);
+        innerTopPanel.add(browseButton,BorderLayout.LINE_END);
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(innerTopPanel, BorderLayout.NORTH);
+    }
 
 }
