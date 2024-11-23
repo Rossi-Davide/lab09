7ac6ff309cc433e14d4ea94b04dc2aa0b6ef926b
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-23 13:45:29 +0100

    Define Controller interface

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,16 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+    void setStringToPrint(String inpuString) throws NullPointerException;
 
+    String getNextStringToPrint();
+
+    List<String> getHistory();
+
+    void printString() throws IllegalStateException;
 }
