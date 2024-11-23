f9028750d938c294830ad8703c1355945c64db5e
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-23 21:49:23 +0100

    Restored to version without GUI interface

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,10 +1,8 @@
 package it.unibo.mvc;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import it.unibo.mvc.api.GUI;
 import it.unibo.mvc.panels.TextEditorPanel;
 
 import java.awt.Dimension;
@@ -14,7 +12,7 @@ import java.awt.Toolkit;
  * A very simple program using a graphical interface.
  * 
  */
-public final class SimpleGUI implements GUI{
+public final class SimpleGUI {
 
     private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame();
@@ -29,19 +27,17 @@ public final class SimpleGUI implements GUI{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    @Override
-    public void display() {
+
+    /**
+     * Sets the size of the frame and pushes it to the view.
+     */
+    private void display() {
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
         frame.setSize(sw / PROPORTION, sh / PROPORTION);
         frame.setLocationByPlatform(true);
         frame.setVisible(true);
-    }
-
-    @Override
-    public void errorDialog(String error) {
-        JOptionPane.showMessageDialog(frame, error, "ERROR", JOptionPane.ERROR_MESSAGE);
     }
 
     /**
@@ -51,4 +47,5 @@ public final class SimpleGUI implements GUI{
     public static void main(final String[] args) {
         new SimpleGUI(new Controller()).display();
     }
+
 }
